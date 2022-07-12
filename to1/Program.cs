237c50a29d1a9386978527aad7cd6a2e9465217e
Rishat <risatnurullin53@gmail.com>
2022-07-12 15:46:50 +0300

    to1

---
+++ to1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите 1-ое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 2-ое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2 )
+{
+    Console.WriteLine("Первое число большого второго ");
+}
+else
+{
+    Console.WriteLine("Второе число большого первого ");
+}
